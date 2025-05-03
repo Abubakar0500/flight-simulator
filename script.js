83f8d6cc9a7d5dcd063dd8d9dda54136375433d1
Abubakar0500 <abubakarnahdi5334@gmail.com>
2025-05-04 05:00:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function fly() {
-  alert("Taking off! ✈️");
+let plane;
+
+function setup() {
+  createCanvas(800, 400);
+  plane = createVector(width / 2, height / 2);
+}
+
+function draw() {
+  background(135, 206, 250); // Sky blue
+  fill(255, 0, 0); // Red plane
+  rect(plane.x, plane.y, 60, 20); // Draw plane
+
+  if (keyIsDown(LEFT_ARROW)) plane.x -= 5;
+  if (keyIsDown(RIGHT_ARROW)) plane.x += 5;
+  if (keyIsDown(UP_ARROW)) plane.y -= 5;
+  if (keyIsDown(DOWN_ARROW)) plane.y += 5;
 }
